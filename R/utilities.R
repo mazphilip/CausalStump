6132d902a25f6b9d069270e0c34c4c66b9d068f6
mazphilip <philip@pilgerstorfer.at>
2017-08-09 10:56:35 +0100

    removed a bug and set up for out-of-sample normalization

--- R/utilities.R
+++ R/utilities.R
@@ -1,21 +1,26 @@
-norm_variables <- function(y,X){
+norm_variables <- function(y,X,moments){
   p = ncol(X)
-  moments = list(meanX = rep(0,p),
-                 varX  = rep(1,p),
-                 meanY  = 0*mean(y), # we do not center the outcome surface
-                 varY  = var(y))
 
-  #only normalize non-binary variables
-  nonbinary_varbs = unique(1:p * (apply(X,2,function(x) { all(na.omit(x) %in% 0:1) })==0))
-  nonbinary_varbs = nonbinary_varbs[nonbinary_varbs!=0]
+  #use function for both in and out-of-sample normalization
+  if(missing(moments)){
+    moments = list(meanX = rep(0,p),
+                   varX  = rep(1,p),
+                   meanY  = 0*mean(y), # we do not center the outcome surface
+                   varY  = var(y))
 
-  tmp = data.frame(X[,nonbinary_varbs])
+    #only normalize non-binary variables
+    nonbinary_varbs = unique(1:p * (apply(X,2,function(x) { all(na.omit(x) %in% 0:1) })==0))
+    nonbinary_varbs = nonbinary_varbs[nonbinary_varbs!=0]
 
-  moments$meanX[nonbinary_varbs] = apply(tmp,2,mean)
-  moments$varX[nonbinary_varbs]  = sqrt(apply(tmp,2,var))
+    tmp = data.frame(X[,nonbinary_varbs])
+
+    moments$meanX[nonbinary_varbs] = apply(tmp,2,mean)
+    moments$varX[nonbinary_varbs]  = apply(tmp,2,var)
+  }
 
   y = (y - moments$meanY )/sqrt(moments$varY)
   mynorm <- function(i){ (X[,i]-moments$meanX[i]) / sqrt(moments$varX[i]) }
+
   X = data.frame(sapply(1:p,mynorm))
 
   list(moments = moments, y=y, X=X)
